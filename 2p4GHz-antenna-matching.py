a3f1a6a36b687ee8ffacd19671014cdc6a1e9014
Stefan Wyss <wyss@superspider.net>
2017-05-19 22:39:02 +0200

    commented code

--- 2p4GHz-antenna-matching.py
+++ 2p4GHz-antenna-matching.py
@@ -9,6 +9,19 @@ import math
 import cmath
 import pylab as pl
 
+# initialize value range of components (in 
+Cp1s = pl.frange(0.1e-12,5.0e-12,0.1e-12)
+Ls1s = pl.frange(0.1e-9,10e-9,0.1e-9)
+Cp2s = pl.frange(0.1e-12,5.0e-12,0.1e-12)
+
+# initialize bounding frequencies
+w_low =  2*math.pi*2.4e9
+w_mid =  2*math.pi*2.44e9
+w_high = 2*math.pi*2.48e9
+
+# initialize Zant antenna impedances (measured with a VNA)
+Z0 = {w_low:complex(79,67),w_mid:complex(116,-70),w_high:complex(31,-55)}
+
 Zw = complex(50,0)
 
 def Zres(R,w): return complex(R,0)
@@ -23,7 +36,7 @@ print("2p4GHz-antenna-matching.py")
 print("--------------------------")
 print("")
 print("Matching circuit is PI-filter:")
-print(" ----Ls1--------------")
+print(" -----Ls1-------------")
 print("  |          |       |")
 print("  |          |       |")
 print(" Cp1        Cp2    Zant")
@@ -31,25 +44,18 @@ print("  |          |       |")
 print("  |          |       |")
 print(" ---------------------")
 
-Cp1_all = pl.frange(0.1e-12,4.0e-12,0.1e-12)
-Ls1_all = pl.frange(0.1e-9,6.0e-9,0.2e-9)
-Cp2_all = pl.frange(0.1e-12,4.0e-12,0.1e-12)
-
-w_low =  2*math.pi*2.4e9
-w_mid =  2*math.pi*2.44e9
-w_high = 2*math.pi*2.48e9
-Z0 = {w_low:complex(79,67),w_mid:complex(116,-70),w_high:complex(31,-55)}
+# do not change anything below here!
 S11 = {w_low:complex(0,0),w_mid:complex(0,0),w_high:complex(0,0)}
 Z = {}
-# goes to 31-j53 Cp1=1.0pF Ls1=4.8nH Cp2=0.7pF
-S11_min = 1000
+Zmin = {}
+S11_min = float("inf")
 
-for Cp2 in Cp2_all:
-    # print('-Cp2={:.2e}'.format(Cp2))
-    for Ls1 in Ls1_all:
-        # print('--Ls1={:.2e}'.format(Ls1))
-        for Cp1 in Cp1_all:
-            # print('---Cp1={:.2e}'.format(Cp1))
+print('Total of {:d} iterations to calculate'.format(len(Cp1s)*len(Ls1s)*len(Cp2s)*len(Z0)))
+
+# iterate over all Cp2s, Ls1s and Cp1s
+for Cp2 in Cp2s:
+    for Ls1 in Ls1s:
+        for Cp1 in Cp1s:
             S11_new = 0
             for w in Z0:
                 Z[w] = par(Z0[w],Zcap(Cp2,w))
@@ -57,13 +63,12 @@ for Cp2 in Cp2_all:
                 Z[w] = par(Z[w],Zcap(Cp1,w))
                 S11[w] = S11_abs(Z[w])
                 S11_new += S11[w]**2
-                # print('    w={:.3e} Z[w].real={:.3f} S11[w]={:.3f}'.format(w,Z[w].real,S11[w]))
-                # print('    S11_new=={:.3f}'.format(S11_new))
 
             if S11_new < S11_min:
                 S11_min = S11_new
+                Zmin = Z.copy()
                 print('New min: Cp1={:.2e} Ls1={:.2e} Cp2={:.2e} S11_min={:.5f}'.format(Cp1,Ls1,Cp2,S11_min))
-        
+                print('Z[w]={:.2f},{:.2f},{:.2f}'.format(Zmin[w_low],Zmin[w_mid],Zmin[w_high]))
     
 
 
